2d0f271935e59013afc73cc598e4b618206099b5
stephenhadi <stephenhadi123@gmail.com>
2020-03-28 18:07:34 +0700

    server and client finished

--- Client.py
+++ Client.py
@@ -1,5 +1,8 @@
 import socket
 import json
+from threading import Timer
+import _thread
+import random
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -7,16 +10,31 @@ BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+nama = input("masukkan nama:")
+s.send(bytearray(nama, 'utf-8'))
 while True:
     data = s.recv(BUFFER_SIZE)
+    if data.decode() == "finish":
+        print("break")
+        break
     print(data.decode())
-    jawaban = input(" ")
-    time = 10 #chris ini ntr lo tentuin gimana cara itung timenya cuman kurang lebih gini
-    result = {
-        "jawaban":jawaban,
-        "time":time
-    }
-    result = json.dumps(result)
-    print(result)
-    s.send(bytearray(result,'utf-8'))
+    t = Timer(20, _thread.interrupt_main)
+    try:
+        t.start()
+        jawaban = input("Ketik jawaban A,B,C,D  dalam 10 detik\n")
+        time = str(random.sample(range(1,10),1))[1:-1]  # chris ini ntr lo tentuin gimana cara itung timenya cuman kurang lebih gini
+        result = {
+            "jawaban": jawaban,
+            "time": int(time)
+        }
+        result = json.dumps(result)
+        s.send(bytearray(result, 'utf-8'))
+    except KeyboardInterrupt:
+        print("timeout")
+    skor = s.recv(BUFFER_SIZE)
+    print("total skor: ", skor.decode(), "\n")
+    t.cancel()
+skorAkhir = s.recv(BUFFER_SIZE)
+skorAkhir = skorAkhir.decode() # ini lo print skor akhir
+print("Break")
 
--- server.py
+++ server.py
@@ -3,58 +3,92 @@ import _thread
 import threading
 from listSoal import listSoal
 import json
+import time
+
 
 def listen():
     counter=0
     while True:
-        global playerConn
-        global playerAddr
+        global listPlayer
         conn, addr = s.accept()
+        nama = conn.recv(1024)
+        nama = nama.decode()
         global msg
         if msg == "begin":
-            break
+            break #when begin no user can join
         counter += 1
-        print("user ", counter, ": ", addr)
-        playerConn.append(conn)
-        playerAddr.append(addr)
+        print("user ", nama, ": ", addr)
+        player = {
+            "nama": nama,
+            "conn": conn,
+            "addr": addr,
+            "totalSkor": 0
+        }
+        listPlayer.append(player)
 
 
-def recieve_data(conn):
-    counter=0
-    data = conn.recv(1024)
+def recieve_data(indeks, player,jawaban):
+    data = player["conn"].recv(1024)
     recieve = json.loads(data.decode())
-    print(recieve["jawaban"])
+    if jawaban.casefold() == recieve["jawaban"].casefold() : #wrong answer is thrown away
+        result = {
+            "indeks": indeks,
+            "time": recieve["time"]
+        }
+        global answer
+        answer.append(result)
 
 
 
 
-global playerConn
-global playerAddr
+global listPlayer
 global msg
+global answer
+answer = []
 msg=""
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((TCP_IP,TCP_PORT))
 s.listen(5)
-playerConn=[]
-playerAddr=[]
-_thread.start_new_thread(listen,())
+listPlayer = []
+listThread = []
+_thread.start_new_thread(listen, ()) #wait for player
 msg = input("write 'begin' to start \n")
 listSoal = listSoal()
 arrOfSoal = listSoal.fetchSoal()
-thread = threading.Thread(target=recieve_data,args=(playerConn[0],))
-thread.start()
-for x in range(0,playerConn.__len__()):
-    break
 
 for x in range(0,arrOfSoal.__len__()):
     soal = arrOfSoal[x].getSoal()
     pilihan = arrOfSoal[x].getPilihan()
     concate = soal + "\n".join(pilihan) +"\n"
-    for y in range(0,playerConn.__len__()):
-        playerConn[y].send(bytearray(concate,'utf-8'))
-    thread.join()
-    msg2 = input("write 'next' untuk ke soal selanjutnya")
-
-
+    listThread=[]
+    for y in range(0, listPlayer.__len__()):
+        listThread.append(threading.Thread(target=recieve_data, args=(y,listPlayer[y],arrOfSoal[x].getJawaban(),)))
+        listThread[y].start() #start thread for recieve answer
+    for y in range(0,listPlayer.__len__()):
+        listPlayer[y]["conn"].send(bytearray(concate,'utf-8'))#send question
+    absolute_time_out = time.time() + 20
+    for y in range(0, listPlayer.__len__()):
+        timeout = absolute_time_out-time.time()
+        if timeout<0:
+            break
+        listThread[y].join(timeout) #waiting
+    answer = sorted(answer, key=lambda k: k["time"])
+    if answer != []:
+        listPlayer[answer[0]["indeks"]]["totalSkor"]+= 50 #+50 point for the fastest answer
+    for y in range(0,listPlayer.__len__()):
+        skor = listPlayer[y]["totalSkor"]
+        listPlayer[y]["conn"].send(bytearray(str(skor),'utf-8')) #send user current score
+        if x == arrOfSoal.__len__()-1:
+            listPlayer[y]["conn"].send(bytearray("finish",'utf-8')) #send code the game is finished
+    if x == arrOfSoal.__len__() - 1:
+        break
+    msg2 = input("write 'next' untuk ke soal selanjutnya \n")
+    answer=[]
+result ="Leaderboard \n"
+for x in range(0,listPlayer.__len__()):
+    result+= listPlayer[x]["nama"]+" : "+str(listPlayer[x]["totalSkor"]) +"\n" #print all leaderboards
+print(result)
+for x in range(0,listPlayer.__len__()):
+    listPlayer[x]["conn"].send(bytearray(result,'utf-8')) #send all result
